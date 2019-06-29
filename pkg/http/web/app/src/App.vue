a2c1ffa89534465c5b2437bc33c6100318c0d031
ThisIsVieTTran <masterqvm@aol.com>
2019-06-29 00:56:38 -0400

    Navbar implemented

--- pkg/http/web/app/src/App.vue
+++ pkg/http/web/app/src/App.vue
@@ -1,17 +1,20 @@
 <template>
 <v-app>
-
+  <toolbar></toolbar>
   <v-content>
     <router-view></router-view>
   </v-content>
-
 </v-app>
 </template>
 
 <script>
+// Make sure to add your components here!
+import Toolbar from '@/components/Toolbar'
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Toolbar
+  },
   data() {
     return {
       //
---
+++ pkg/http/web/app/src/components/Toolbar.vue
@@ -0,0 +1,62 @@
+<template>
+<nav>
+
+
+  <v-toolbar app>
+    <v-toolbar-side-icon @click="drawerVis = !drawerVis"></v-toolbar-side-icon>
+    <v-toolbar-title>SomeApp</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Descriptive</v-btn>
+      <v-btn flat>Predictive</v-btn>
+      <v-btn flat>Prescriptive</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+
+  <v-navigation-drawer v-model="drawerVis" app>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Application
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-divider></v-divider>
+
+    <v-list dense class="pt-0">
+      <v-list-tile v-for="item in items" :key="item.title">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+
+</nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      drawerVis: false,
+      items: [{
+          title: 'Home',
+          icon: 'dashboard'
+        },
+        {
+          title: 'About',
+          icon: 'question_answer'
+        }
+      ],
+      right: null
+    }
+  }
+}
+</script>
